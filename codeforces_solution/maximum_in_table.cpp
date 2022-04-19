0e86cd8f50af07fd9994144cd02c57ce71a72543
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-04-18 20:14:09 -0400

    Started with 800 rated problem 509A

---
+++ codeforces_solution/maximum_in_table.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<int>>arr(n, vector<int>(n, 0));
+    for(int i = 0; i < n; ++i){
+        arr[0][i] = 1;
+        arr[i][0] = 1;
+    }
+    for(int i = 1; i < n; ++i) {
+        for(int j = 1; j < n; ++j) {
+            arr[i][j] = arr[i-1][j] + arr[i][j-1];
+        }
+    }
+    cout << arr[n-1][n-1] << endl;
+}
+
+
+int main() {
+    solve();
+}
