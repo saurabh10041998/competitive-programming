1def094c07573f69d29196000d01097921c39826
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-05-19 12:36:59 -0400

    Approach ======== We cannot make pair of (1,2).. otherwise sum will be less and we have make pair of type (2,1) to increase the sum.
    Target --> Is to maximize (2, 1) and minimize (1, 2) pair
    
    Now pairs (1,0), (0,1), (2,0) and (0,2) will not contribute any to the sum.
    
    So first reduce those..
    
    But it would be wise to reduce pair (1, 0) and (0, 2)
    because of it will reduce number of 1 in seq1 and number of 2
    in seq, Thus reducing 1,2 pairs
    
    so let's say array is x0, x1, x2 and y0, y1, y2
    
    So number of (1, 0) pair is
    
    min(x1, y0) -->a1
    
    x1 - a1
    
    y0 - a1
    
    Same for (0,2)
    
    min(x0, y2) --> a2
    
    x0 - a2
    y2 - a2
    
    Now we got (1,2) (minimized by above operation) and (2, 1)
    
    Number of (2, 1)
    
    min(x2, y1) --> a3
    
    sum = 2 * a3
    
    and min (x1, y2) --> a4
    
    sum = - 2 * a4
    
    sum is our ans. :)

---
+++ codeforces_solution/1401_b_ternery_sequence.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    int x0, x1, x2, y0, y1, y2;
+    cin >> x0 >> x1 >> x2 >> y0 >> y1 >> y2;
+
+    int a1 = std::min(x1, y0);
+    x1 -= a1;
+    y0 -= a1;
+
+    int a2 = std::min(x0, y2);
+    x0 -= a2;
+    y2 -= a2;
+    
+    int a21 = std::min(x2, y1);
+    x2 -= a21;
+    y1 -= a21;
+
+    int sum = 2 * a21;
+    
+    int a12 = std::min(x1, y2);
+
+    sum -= 2 * a12;
+
+    cout << sum << endl;
+}
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
