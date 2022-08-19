f9bc61f7dd6db07609622573c195677a735297aa
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-08-19 07:00:13 -0400

    more problemzz, day2.. 12 to be exact

---
+++ codeforces_solution/862a_mahmoud_and_ehab_and_the_mex.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    int n, x;
+    cin >> n >> x; 
+    vector<int>nums(n);
+    vector<bool>check(101, false);
+    for(int i = 0; i < n; ++i) {
+        cin >> nums[i];
+        check[nums[i]] = true;
+    }
+    int cnt = 0;
+    if(check[x] == true) cnt = 1;
+    for(int i = 0; i < x; ++i) {
+        if(!check[i]) cnt++;
+    }
+    cout << cnt << endl;
+}
+
+int main() {
+    double start = clock();
+    int tc = 1;
+   // cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
