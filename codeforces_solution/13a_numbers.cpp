dae9ea63429848a6dcfe2fcb25cadb2f23ca5d1d
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-08-22 21:33:14 -0400

    day4, problems 9

---
+++ codeforces_solution/13a_numbers.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int gcd(int x, int y) { 
+    int r = x % y; 
+    while(r != 0) { 
+        x = y; 
+        y = r;
+        r = x % y;
+    }
+    return y;
+}
+
+void solve() {
+    int n;
+    cin >> n; 
+    int ds(0), Y(n - 2);
+    for(int i = 2; i <= n - 1; ++i) {
+        int k(n); 
+        while(k != 0) { 
+            ds += k % i; 
+            k /= i;
+        }
+    }
+    int g = gcd(ds, Y);
+    ds /= g;
+    Y /= g;
+    cout << ds << "/" << Y << endl;
+}
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
