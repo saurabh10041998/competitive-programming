e7e2721c0eb50f68ec3ba3d41c833c2452c3fbc9
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-08-23 18:49:18 -0400

    day5, problems 5, rating 1100

---
+++ codeforces_solution/349a_cinema_line.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int>arr(n);
+    for(int i = 0; i < n; ++i)
+        cin >> arr[i];
+    int twenty_fives = 0;
+    int fifties = 0;
+    bool ok = true;
+    for(int i = 0; i < n; ++i) {
+        if(arr[i] == 25) {
+            twenty_fives++;
+        }else if(arr[i] == 50) {
+            if(twenty_fives > 0) {
+                twenty_fives--;
+                fifties++;
+            }else {
+                ok = false;
+                break;
+            }
+        }else {
+            if(fifties > 0 && twenty_fives > 0) {
+                fifties--;
+                twenty_fives--;
+            }else if(twenty_fives >= 3) {
+                twenty_fives -= 3;
+            }else {
+                ok = false;
+                break;
+            }
+        }
+    }
+    cout << (ok ? "YES" : "NO") << endl;
+}
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
