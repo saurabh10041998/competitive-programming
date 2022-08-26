af1330ce4c4857ca95713b3030bd15317ec5ce8d
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-08-25 21:44:39 -0400

    day7, problems 5, rating 1100

---
+++ codeforces_solution/1624c_division_by_two_and_permutation.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    int n; 
+    cin >> n;
+    vector<int>arr(n);
+    vector<bool>used(n + 1, false);
+    for(int i = 0; i < n; ++i)
+        cin >> arr[i];
+    std::sort(all(arr), [](int a, int b) {
+        return a > b;        
+    });
+
+    bool ok = true;
+
+    for(int &i : arr) {
+        int x = i;
+        while(x > n || used[x]) x /= 2;
+        if(x > 0)
+            used[x] = true;
+        else {
+            ok = false;
+            break;
+        }
+    }
+    cout << (ok ? "YES" : "NO") << endl;
+}
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
