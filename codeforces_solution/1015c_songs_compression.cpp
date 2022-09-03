ca60acdc4b70f05cd6676c4a1c82230d9e08b7f8
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-09-03 05:37:16 -0400

    day13, problems5

---
+++ codeforces_solution/1015c_songs_compression.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve() {
+    ll n, m; 
+    cin >> n >> m;
+    vector<pair<ll, ll>>arr(n);
+    ll sum = 0;
+    for(ll i = 0; i < n; ++i) { 
+        cin >> arr[i].first >> arr[i].second;
+        sum += arr[i].first;    
+    }
+    std::sort(arr.begin(), arr.end(), [&](pair<ll, ll>a, pair<ll, ll>b) -> bool{
+        return a.first - a.second > b.first - b.second;     
+    });
+    
+    ll ans = -1;
+    for(ll i = 0; i < n; ++i) {
+        if(sum <= m) {
+            ans = i;
+            break;
+        }
+        sum -= (arr[i].first - arr[i].second);
+    }
+    
+    if(sum <= m){
+        if(ans != -1) {
+            cout << ans << endl;
+        }else {
+            cout << n << endl;
+        }
+    }else {
+        cout << -1 << endl;
+    }
+}    
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
