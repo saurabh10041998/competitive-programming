357a49cf390b2f1ca5147374a34dd949cb9de1af
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-09-05 00:52:35 -0400

    day15,problems 4

---
+++ codeforces_solution/1189b_number_circle.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    ll n; 
+    cin >> n;
+    vector<ll>arr(n);
+    for(ll i = 0; i < n; ++i)
+        cin >> arr[i];
+    std::sort(all(arr));
+    // since an only going to have its consecutive members
+    if(arr[n - 1] >= arr[n - 2] + arr[n - 3]){
+        cout << "NO" << endl;
+    }else { 
+        cout << "YES" << endl;
+        for(ll i = n - 1; i >= 0; i -= 2)
+            cout << arr[i] << " ";
+        for(ll i = n % 2; i < n; i += 2)
+            cout << arr[i] << " \n"[i == n - 2];
+    }
+}     
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
---
+++ codeforces_solution/581b_luxurious_houses.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    int n;
+    cin >> n; 
+    vector<int>arr(n);
+    for(int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    vector<int>ans(n);
+    int max = 0;
+    for(int i = n - 1; i >= 0; --i) {
+        ans[i] = std::max(0, max - arr[i]  + 1);
+        max = std::max(max, arr[i]);
+    }
+    for(int i = 0; i < n; ++i)
+        cout << ans[i] << " \n"[i == n - 1];
+}    
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
