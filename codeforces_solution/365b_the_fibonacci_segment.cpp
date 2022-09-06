322306040284a6fdc3f397c44575ed14a0cecfac
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-09-05 22:01:51 -0400

    day16,problems 5

---
+++ codeforces_solution/365b_the_fibonacci_segment.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    ll n; 
+    cin >> n; 
+    vector<ll>arr(n); 
+    for(ll i = 0; i < n; ++i){
+        cin >> arr[i];
+    }
+    if(n  < 2 ) {
+        cout << n << endl; 
+        return;
+    }
+    ll cnt = 2;
+    ll ans = 2;
+    for(ll i = 2; i < n; ++i) { 
+        if(arr[i] == arr[i - 1] + arr[i - 2])
+            cnt++;
+            
+        else {
+            ans = std::max(ans, cnt);
+            cnt = 2;
+        }
+    }
+    ans = std::max(ans, cnt); 
+    cout << ans << endl;
+}    
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
