7856123fff0f3d8098affaaedcca26dc94ed029b
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-09-07 11:00:34 -0400

    day17, problem6

---
+++ codeforces_solution/1672c_unequal_array.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll>arr(n);
+    ll eq = 0;
+    //set<ll>st; 
+    ll prev;
+    for(ll i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    ll mn = -1; 
+    ll mx = -1;
+    for(ll i = 1; i < n; ++i) { 
+        if(arr[i] == arr[i - 1]) { 
+            if(mn == -1) { 
+                mn = i;
+            }
+            mx = i;
+        }
+    }
+    if(mn == mx) { 
+        cout << 0 << endl;
+    }else { 
+        cout << std::max(1LL, mx - mn - 1) << endl;
+    }
+}    
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
