bdac79c0f003cd91353d447556f1f6d2c8df51c4
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-09-25 09:59:48 -0400

    day35, problems1, rating 1300

---
+++ codeforces_solution/1300/1650d_twist_the_permutation.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void leftRotate(vector<int>&arr, int n, int k) { 
+    k %= n;
+    std::reverse(arr.begin(), arr.begin() + k);
+    std::reverse(arr.begin() + k , arr.begin() + n);
+    std::reverse(arr.begin(), arr.begin() + n);
+}
+
+void solve() {
+    int n; 
+    cin >> n;
+    vector<int>arr(n);
+    for(int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    vector<int>ans(n);
+    for(int i = n - 1; i >= 0; --i) {
+        int x = 0;
+        for(x = 0; x < i; ++x) {
+            if(arr[x] == i + 1) 
+                break;
+        }
+        if(x  == i)
+            x = -1;
+        leftRotate(arr, i + 1, x + 1);
+        ans[i] = x + 1;
+    }
+    for(int i = 0; i < n; ++i)
+        cout << ans[i] << " ";
+    cout << endl;
+}    
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
