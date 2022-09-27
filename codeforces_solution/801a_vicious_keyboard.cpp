211c2cc16dc18edaea000c0eb1615fb00fa9680f
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-09-26 21:39:36 -0400

    day37, problems 3

---
+++ codeforces_solution/801a_vicious_keyboard.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    string  str;
+    cin >> str;
+    int n = sza(str);
+    int ans = 0;
+    for(int i = 0; i < n -1; ++i) {
+        ans += (str[i] == 'V' && str[i + 1] == 'K');
+    }
+    int cnt = 0;
+    for(int p = 0; p < n; ++p) { 
+        str[p] = 'V' + 'K' - str[p];
+        cnt = 0;
+        for(int i = 0; i < n - 1; ++i) { 
+            cnt += (str[i] == 'V' && str[i + 1] == 'K');
+        }
+        ans = std::max(ans, cnt);
+        str[p] = 'V' + 'K' - str[p];
+    }
+    cout << ans  << endl;
+}    
+
+int main() {
+    double start = clock();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+       solve();
+    }
+    cerr << "[*] Time: " << (clock() * 1.0 - start) / CLOCKS_PER_SEC << std::setprecision(3) << " s" << endl;
+    return 0;
+}
