4311bba7e4a5a62c337f3a76f454ae7eca2676d1
saurabh Shinde <37367457+saurabh10041998@users.noreply.github.com>
2022-11-17 10:15:31 +0530

    Session cookie hide

--- aoc_solution/day1/src/get_input.py
+++ aoc_solution/day1/src/get_input.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import requests
 
-cookies = {"session": "53616c7465645f5f2ce9102ace19138016bc33216dbae16d73fce4fd5bffb9e1db4463a0115920e23071df98381b24c5f8d675b86a8126a311e8d00259f327b7"}
+cookies = {"session": "<session_cookie_here>"}
 
 url = "https://adventofcode.com/2015/day/1/input"
 
