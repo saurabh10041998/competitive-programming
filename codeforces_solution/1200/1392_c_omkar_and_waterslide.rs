775bd37184c028726cbf7453f52324bfd7ec6356
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-11-29 21:18:55 -0500

    day6, question1, rust solid start

---
+++ codeforces_solution/1200/1392_c_omkar_and_waterslide.rs
@@ -0,0 +1,52 @@
+#[allow(unused_macros)]
+macro_rules! read {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).expect("A string");
+        let $out = inner.trim().parse::<$type>().expect("A parsable");
+    };
+}
+#[allow(unused_macros)]
+macro_rules! read_str {
+    ($out:ident) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).expect("A string");
+        let $out = inner.trim();
+    };
+}
+#[allow(unused_macros)]
+macro_rules! read_vec {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).expect("A string");
+        let $out = inner
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<$type>().unwrap())
+            .collect::<Vec<$type>>();
+    };
+}
+fn solve() {
+    read!(n as usize);
+    read_vec!(arr as i64);
+    let mut arr = arr;
+    let (mut last, mut ans) =  (0, 0);
+    for i in 0..n {
+        arr[i] += ans;
+        if arr[i] >= last {
+            last = arr[i];
+        }else {
+            ans += last - arr[i];
+        }
+    }
+    println!("{}", ans);
+}
+fn main() {
+    //let t = 1;
+    read!(t as i32);
+    let mut t = t;
+    while t > 0 {
+        solve();
+        t -= 1;
+    }
+}
